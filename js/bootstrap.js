a2efb902fcae9ca3c8ba429778668170c50d8b44
山野 真善志 <yamano@psid.co.jp>
2018-01-05 19:52:44 +0900

    [FIX]アニメ、ライトボックス追加

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2375,3 +2375,24 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+
+$(function() {
+    var topBtn = $('#page-top');    
+    topBtn.hide();
+    //スクロールが500に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
